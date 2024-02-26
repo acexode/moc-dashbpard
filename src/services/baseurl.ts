9b326b743059e73c89113ec6df4f7b20aaf7e8a2
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2024-02-26 16:05:20 +0100

    updated baseurl

--- src/services/baseurl.ts
+++ src/services/baseurl.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = `${import.meta.env.VITE_BASE_URL}`;
-export const MOC_BASE_URL = `https://stingray-app-g5w3h.ondigitalocean.app`;
+export const MOC_BASE_URL = `http://64.23.181.142:3000`;
