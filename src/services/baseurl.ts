f27d7f09b7273da970bc48450f08facadd426b95
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2024-02-27 00:28:12 +0100

    Update baseurl.ts

--- src/services/baseurl.ts
+++ src/services/baseurl.ts
@@ -1,2 +1,3 @@
-export const BASE_URL = `${import.meta.env.VITE_BASE_URL}`;
+// export const BASE_URL = `${import.meta.env.VITE_BASE_URL}`;
+export const BASE_URL = `http://localhost:3000/proxy-data`;
 export const MOC_BASE_URL = `http://64.23.181.142:3000`;
